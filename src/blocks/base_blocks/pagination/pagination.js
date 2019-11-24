c826aa5971b6e2c72a9e492de6beb1c88d3845e4
Polina <ipolina33@gmail.com>
2019-11-24 16:41:11 +0500

    added pagination, not finished

---
+++ src/blocks/base_blocks/pagination/pagination.js
@@ -0,0 +1,2 @@
+//$('.pagination__listItem:nth-child(2)').attr('class', 'pagination__link_current');
+$('.pagination__listItem:nth-child(2)').addClass('pagination__link_current');
--- src/blocks/pages/ui-kit.js
+++ src/blocks/pages/ui-kit.js
@@ -3,3 +3,4 @@
 //})
 import '../base_blocks/inputs/dropdown/dropdown'
 import '../base_blocks/expandableCheckboxList/expandableCheckboxList'
+import '../base_blocks/pagination/pagination'
