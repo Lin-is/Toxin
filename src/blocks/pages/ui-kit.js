ae5f115a1bd458e92bccac6d1ef9f9aa827b5f84
Polina <ipolina33@gmail.com>
2019-11-25 11:18:14 +0500

    added js for likeButton

--- src/blocks/pages/ui-kit.js
+++ src/blocks/pages/ui-kit.js
@@ -4,3 +4,4 @@
 import '../base_blocks/inputs/dropdown/dropdown'
 import '../base_blocks/expandableCheckboxList/expandableCheckboxList'
 import '../base_blocks/pagination/pagination'
+import '../base_blocks/buttons/likeButton/likeButton'
