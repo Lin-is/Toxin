c194b1bfbbafcedfa565614f2cd59968aca95df2
Polina <ipolina33@gmail.com>
2019-12-16 18:44:45 +0500

    was corrected dateDropdown, added styles for air-datepicker

--- src/blocks/base_blocks/inputs/dateDropdown/dateDropdown.js
+++ src/blocks/base_blocks/inputs/dateDropdown/dateDropdown.js
@@ -1,20 +1,35 @@
 import 'air-datepicker'
 
 let elements = document.querySelectorAll('.dateDropdown__input')
-console.log(elements)
 let dateDropdowns = []
-for (let elem of elements) {
-    dateDropdowns.push(document.getElementById(elem.getAttribute('id')))
+let secondInputs = []
+
+for (let i = 0; i < elements.length; i++) {
+    let temp = $(elements[i]).parent().siblings('.dateDropdown__label').children('.dateDropdown__secondDateInput')[0];
+    let firstInput =  elements[i].getAttribute('id');
+    let secondInput = temp.getAttribute('id');
+    dateDropdowns.push(firstInput);
+    secondInputs.push(secondInput);
 }
-console.log(dateDropdowns)
-for(let elem of dateDropdowns){
-    $('#' + elem.getAttribute('id')).datepicker({
+
+for(let i = 0; i < dateDropdowns.length; i++){
+    $('#' + dateDropdowns[i]).datepicker({
         classes: 'added_datepicker',
         nextHtml: '<i class="material-icons">arrow_forward</i>',
         prevHtml: '<i class="material-icons">arrow_back</i>',
+        navTitles: {
+            days: 'MM <i>yyyy</i>',
+        },
+        range: true,
+        multipleDatesSeparator: '-',
+        onSelect: function (fd, d, picker) { 
+            $('#' + dateDropdowns[i]).val(fd.split("-")[0]);
+            $('#' + secondInputs[i]).val(fd.split("-")[1]);
+        },
         clearButton: true,
     })
-    console.log('SSSSSSSSSSS')
+    let dp = $('#' + dateDropdowns[i]).datepicker().data('datepicker');
+    $('#' + secondInputs[i]).click(function (){
+        dp.show();
+    })
 }
-
-
