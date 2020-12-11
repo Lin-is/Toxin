019873f5bd7398a6db7df296f9677b44a999ea1e
Polina <ipolina33@gmail.com>
2020-12-11 16:13:09 +0500

    some fixes

--- src/blocks/base_blocks/header/header.js
+++ src/blocks/base_blocks/header/header.js
@@ -4,11 +4,11 @@ let headers = document.querySelectorAll('.header__navigationContainer');
 $('.header__navigationButton').click(function () {
     if(this.classList.contains('header__navigationButton_clicked')) {
         this.classList.remove('header__navigationButton_clicked');
-        $(this).parent().siblings('.header__navigationContainer').removeClass('header__navigationContainer_open')
+        $(this).siblings('.header__navigationContainer').removeClass('header__navigationContainer_open')
     } 
     else {
         this.classList.add('header__navigationButton_clicked');
-        $(this).parent('.header__buttonsContainer').siblings('.header__navigationContainer').addClass('header__navigationContainer_open')
+        $(this).siblings('.header__navigationContainer').addClass('header__navigationContainer_open');
     }
-        
-})
+});
+
