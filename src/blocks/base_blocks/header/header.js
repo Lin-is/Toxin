39f17adeb52062c0099852ad3eaee6867dedd435
Polina <ipolina33@gmail.com>
2020-12-11 23:40:15 +0500

    some fixes

--- src/blocks/base_blocks/header/header.js
+++ src/blocks/base_blocks/header/header.js
@@ -4,11 +4,13 @@ let headers = document.querySelectorAll('.header__navigationContainer');
 $('.header__navigationButton').click(function () {
     if(this.classList.contains('header__navigationButton_clicked')) {
         this.classList.remove('header__navigationButton_clicked');
-        $(this).siblings('.header__navigationContainer').removeClass('header__navigationContainer_open')
+        $(this).siblings('.header__navigationContainer').removeClass('header__navigationContainer_open');
+        $(document.body).removeClass('freeze');
     } 
     else {
         this.classList.add('header__navigationButton_clicked');
         $(this).siblings('.header__navigationContainer').addClass('header__navigationContainer_open');
+        $(document.body).addClass('freeze'); 
     }
 });
 
